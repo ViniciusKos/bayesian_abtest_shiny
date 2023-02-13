923c9e79cc63336480e301985c9b9eca224d1b8e
ViniciusKos <kos.mota28@hotmail.com>
2023-02-13 09:28:19 -0300

    creation of data csv generator

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+import numpy as np
+
 app = Flask( __name__ )
 
 @app.route( '/home' )
 def index():
-    return render_template("page_blue.html")
-    
+    if np.random.random() < 0.5:
+        return render_template("page_blue.html")
+    else:
+        return render_template("page_red.html")
+
+@app.route( "/yes", methods=['POST'] )
+def yes_event():
+    return redirect( url_for( "index" ))
+
+@app.route( "/no", methods=['POST'] )
+def no_event():
+    return redirect( url_for( "index" ))
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
