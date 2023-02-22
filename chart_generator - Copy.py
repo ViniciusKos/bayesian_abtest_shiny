1076a1452b01830ba0bf7150f9b00824c6e493b8
ViniciusKos <kos.mota28@hotmail.com>
2023-02-22 18:06:47 -0300

    create first snippet of bayesian function

--- chart_generator - Copy.py
+++ chart_generator - Copy.py
@@ -14,8 +14,8 @@ def bayesian_inference( data ):
 
 def animate( i ):
     df0 = pd.concat( [ pd.read_csv(f"data_collected_{i}.csv") for i in ["red","blue"] ] )
-    df1 = df0.copy()
-    #data pivot
+
+    df1 = df1.copy()
     for i in ['visit','click']:
         df1[i] = df1[i].astype(int)
     df1 = df1.reset_index().rename( columns={'index':'day'} )
@@ -26,19 +26,22 @@ def animate( i ):
     for i in df1.columns:
         df1[f"acc_{i}"] = df1[i].cumsum()
 
-    
-    # for group in ["control", "treatment"]:
-    #     df_filtered = df1[df1['group']==group]
-    #     df_filtered['click'] = df_filtered['click'].astype(int)
-    #     df_filtered['visit'] = df_filtered['visit'].astype(int)
+
+
+    plt.cla()
+    for group in ["control", "treatment"]:
+
+        df_filtered = df1[df1['group']==group]
+        df_filtered['click'] = df_filtered['click'].astype(int)
+        df_filtered['visit'] = df_filtered['visit'].astype(int)
 
         x1 = np.arange( p )
+        y1 = df_filtered['click'].cumsum()/df_filtered['visit'].cumsum()
 
-        plt.plot( x1, p, label=f"Probability B better A")
-        plt.plot( x1, expected_loss_a, label='Risk Choosing A')
-        plt.plot( x1, expected_loss_b, label='Risk Choosing B')
+        plt.plot( x1, y1, label=f"{group} Group")
         plt.legend( loc='upper left' )
         plt.tight_layout()
+        plt.show()
 
 ani = FuncAnimation( plt.gcf(), animate, interval=1000 )
 
