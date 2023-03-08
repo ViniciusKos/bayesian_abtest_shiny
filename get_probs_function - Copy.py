1accf3c8e4469e2381dcc940883f38dedfc29d5f
ViniciusKos <kos.mota28@hotmail.com>
2023-03-08 05:55:01 -0300

    added dictionaries to proba function

--- get_probs_function - Copy.py
+++ get_probs_function - Copy.py
@@ -6,7 +6,7 @@ from scipy import stats
 
 def get_prob_and_loss( data , sample_size = None):
 
-    dic_val = {}
+    vals = {}
     proba_b_better_a = []
     expected_loss_a = []
     expected_loss_b = []
@@ -18,22 +18,30 @@ def get_prob_and_loss( data , sample_size = None):
 
 
         for v in ["a", "b"]:
-            dic_val[f"u_{v}"], dic_val[f"var_{v}"] = stats.beta.stats( a = 1 + data.loc[day, 'acc_clicks_A'], \
+
+            #Generate Beta distribution
+            vals[f"u_{v}"], vals[f"var_{v}"] = stats.beta.stats( a = 1 + data.loc[day, 'acc_clicks_A'], \
                                     b = 1 + (data.loc[day, 'acc_visits_A'] - data.loc[day, 'acc_clicks_A']), moments='mv')
-            dic_val[f"x_{v}"] = np.random.normal( loc = u_a, scale = 1.25*np.sqrt( var_a ) ,size = sample_size)
-            dic_val[f"f{v}"] = stats.beta.pdf (  dic_val[f"x_{v}"], 
+            
+            #Generate Normal distribution
+            vals[f"x_{v}"] = np.random.normal( loc = u_a, scale = 1.25*np.sqrt( var_a ) ,size = sample_size)
+
+            #Generate Beta PDF
+            vals[f"f{v}"] = stats.beta.pdf (  vals[f"x_{v}"], 
                             a = 1 + data.loc[day, 'acc_clicks_A'], 
                             b = 1 + ( data.loc[day, 'acc_visits_A'] - data.loc[day, 'acc_clicks_A']))
-            dic_val[f"{v}"] = stats.norm.pdf(  dic_val[f"x_{v}"], loc = u_a, scale = 1.25*np.sqrt( var_a ))
+            
+            #Generate Normal PDF
+            vals[f"g{v}"] = stats.norm.pdf(  vals[f"x_{v}"], loc = u_a, scale = 1.25*np.sqrt( var_a ))
 
 
         
         # Beta / Normal
-        y = ( fa*fb ) / ( ga*gb )
-        yb = y[x_b >= x_a]
+        y = ( vals['fa'] * vals['fb'] ) / ( vals['ga'] * vals['gb'] )
+        yb = y[ vals['x_b'] >= vals['x_a']]
         
         #Calculate probabilities
-        p = ( 1/ sample_size ) * np.sum(yb)
+        prob_b_better_a = ( 1/ sample_size ) * np.sum(yb)
 
 
-    return proba_b_better_a, expected_loss_a, expected_loss_b
+    return prob_b_better_a, expected_loss_a, expected_loss_b
--- get_probs_function.py
+++ get_probs_function.py
@@ -6,51 +6,42 @@ from scipy import stats
 
 def get_prob_and_loss( data , sample_size = None):
 
+    vals = {}
     proba_b_better_a = []
     expected_loss_a = []
     expected_loss_b = []
 
 
-
     for day in range( len ( data ) ):
         u_a , var_a = stats.beta.stats( a = 1 + data.loc[day, 'acc_clicks_A'], \
                                     b = 1 + (data.loc[day, 'acc_visits_A'] - data.loc[day, 'acc_clicks_A']), moments='mv')
-        u_b , var_b = stats.beta.stats( a = 1 + data.loc[day, 'acc_clicks_B'], \
-                                    b = 1 + (data.loc[day, 'acc_visits_B'] - data.loc[day, 'acc_clicks_B']), moments='mv')
 
 
-        # Normal distribution Sample A and B
-        x_a = np.random.normal( loc = u_a, scale = 1.25*np.sqrt( var_a ) ,size = sample_size)
-        x_b = np.random.normal( loc = u_b, scale = 1.25*np.sqrt( var_b ) ,size = sample_size)
+        for v in ["a", "b"]:
 
+            #Generate Beta distribution
+            vals[f"u_{v}"], vals[f"var_{v}"] = stats.beta.stats( a = 1 + data.loc[day, 'acc_clicks_A'], \
+                                    b = 1 + (data.loc[day, 'acc_visits_A'] - data.loc[day, 'acc_clicks_A']), moments='mv')
+            
+            #Generate Normal distribution
+            vals[f"x_{v}"] = np.random.normal( loc = u_a, scale = 1.25*np.sqrt( var_a ) ,size = sample_size)
 
-        # Norm distribution function of page A nad page B
-        fa = stats.beta.pdf ( x_a, 
+            #Generate Beta PDF
+            vals[f"f{v}"] = stats.beta.pdf (  vals[f"x_{v}"], 
                             a = 1 + data.loc[day, 'acc_clicks_A'], 
                             b = 1 + ( data.loc[day, 'acc_visits_A'] - data.loc[day, 'acc_clicks_A']))
+            
+            #Generate Normal PDF
+            vals[f"g{v}"] = stats.norm.pdf(  vals[f"x_{v}"], loc = u_a, scale = 1.25*np.sqrt( var_a ))
 
-        fb = stats.beta.pdf ( x_b,
-                            a = 1 + data.loc[day, 'acc_clicks_B'],
-                            b = 1 + ( data.loc[day, 'acc_visits_B'] - data.loc[day, 'acc_clicks_B'] ))
+
         
-
-        # Beta distribution function of page A and B
-        ga = stats.norm.pdf( x_a, loc = u_a, scale = 1.25*np.sqrt( var_a ))
-        gb = stats.norm.pdf( x_b, loc = u_b, scale = 1.25*np.sqrt( var_b ))
-        
-
         # Beta / Normal
-        y = ( fa*fb ) / ( ga*gb )
-        yb = y[x_b >= x_a]
+        y = ( vals['fa'] * vals['fb'] ) / ( vals['ga'] * vals['gb'] )
+        yb = y[ vals['x_b'] >= vals['x_a']]
         
         #Calculate probabilities
-        p = ( 1/ sample_size ) * np.sum(yb)
+        prob_b_better_a = ( 1/ sample_size ) * np.sum(yb)
 
-        expected_loss_A = (1 / sample_size ) * np.sum ( ( ( x_b - x_a )*y) [x_b >= x_a] )
-        expected_loss_B = (1 / sample_size ) * np.sum ( ( ( x_a - x_b )*y) [x_a >= x_b] )
 
-        proba_b_better_a.append(p)
-        expected_loss_a.append( expected_loss_A )
-        expected_loss_b.append( expected_loss_B )
-
-    return proba_b_better_a, expected_loss_a, expected_loss_b
+    return prob_b_better_a, expected_loss_a, expected_loss_b
