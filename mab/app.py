0bf666494239431a6413a0202f9d7f4ce41134f0
ViniciusKos <kos.mota28@hotmail.com>
2023-03-20 10:27:28 -0300

    finish creation of random agent with bayesian test

--- mab/app.py
+++ mab/app.py
@@ -5,21 +5,24 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask( __name__ )
 
-prob_reward=None
+prob_reward = None
+df = pd.read_csv(f"data_experiment.csv")
 
 @app.route( '/home' )
 def index():
 
-    df = pd.read_csv(f"data_experiment.csv")
+    global prob_reward, df
+
     df['no_click'] = df['visit'] - df['click']
     alpha = df.groupby( "group" )[['click', 'no_click']].sum()
 
     print( alpha )
 
-
-    # Thompson Agent
+    # Thompson Agent - Draw random beta sample from pages conversion
     prob_reward = np.random.beta( alpha['click'], alpha['no_click'] )
 
+
+    # check which page has the highest conversion and return it
     if np.argmax( prob_reward ) == 0:
         return render_template( "page_red.html" )
     else:
@@ -28,31 +31,33 @@ def index():
 
 @app.route( "/yes", methods=['POST'] )
 def yes_event():
-    df0 = pd.read_csv("data_experiment.csv" )
+    global prob_reward, df
+
+    # check which page has the highest conversion and create new row
     if np.argmax( prob_reward ) == 0:
         new_row = {"click":1, "visit":1, "group":"control"}
-        df0 = df0.append( new_row, ignore_index=True )
     else:
-        new_row = {"click":1, "visit":1, "group":"treatment"}
-        df0 = df0.append( new_row, ignore_index=True )
+        new_row = {"click":1, "visit":1, "group":"treatment"} 
 
-    df0.to_csv( "data_experiment.csv", index=False)
+    df = df.append( new_row, ignore_index=True )
+    df.to_csv( "data_experiment.csv", index=False)
     return redirect( url_for( "index" ))
 
 
 
 @app.route( "/no", methods=['POST'] )
 def no_event():
-    df0 = pd.read_csv(f"data_experiment.csv")
-    print(request.form )
+    global prob_reward, df
+
+
+    # check which page has the highest conversion and create new row
     if np.argmax( prob_reward ) == 0:
         new_row = {"click":0, "visit":1, "group":"control"}
-        df0 = df0.append( new_row, ignore_index=True )
     else:
         new_row = {"click":0, "visit":1, "group":"treatment"}
-        df0 = df0.append( new_row, ignore_index=True )
 
-    df0.to_csv( "data_experiment.csv", index=False)
+    df = df.append( new_row, ignore_index=True )
+    df.to_csv( "data_experiment.csv", index=False)
     return redirect( url_for( "index" ))
 
 
