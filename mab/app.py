a7cf0a4b23b240a69df2a99223db9504c074b798
ViniciusKos <kos.mota28@hotmail.com>
2023-03-20 10:07:56 -0300

    change if condition app

--- mab/app.py
+++ mab/app.py
@@ -5,17 +5,18 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask( __name__ )
 
+prob_reward=None
+
 @app.route( '/home' )
 def index():
-    # get data
 
     df = pd.read_csv(f"data_experiment.csv")
     df['no_click'] = df['visit'] - df['click']
-
     alpha = df.groupby( "group" )[['click', 'no_click']].sum()
 
-
     print( alpha )
+
+
     # Thompson Agent
     prob_reward = np.random.beta( alpha['click'], alpha['no_click'] )
 
@@ -25,15 +26,14 @@ def index():
         return render_template( "page_blue.html" )
 
 
-
 @app.route( "/yes", methods=['POST'] )
 def yes_event():
     df0 = pd.read_csv("data_experiment.csv" )
-    if request.form['yescheckbox_blue'] =='on':
-        new_row = {"click":1, "visit":1, "group":"treatment"}
+    if np.argmax( prob_reward ) == 0:
+        new_row = {"click":1, "visit":1, "group":"control"}
         df0 = df0.append( new_row, ignore_index=True )
     else:
-        new_row = {"click":1, "visit":1, "group":"control"}
+        new_row = {"click":1, "visit":1, "group":"treatment"}
         df0 = df0.append( new_row, ignore_index=True )
 
     df0.to_csv( "data_experiment.csv", index=False)
@@ -44,11 +44,12 @@ def yes_event():
 @app.route( "/no", methods=['POST'] )
 def no_event():
     df0 = pd.read_csv(f"data_experiment.csv")
-    if request.form['nocheckbox_blue '] =='on':
-        new_row = {"click":0, "visit":1, "group":"treatment"}
+    print(request.form )
+    if np.argmax( prob_reward ) == 0:
+        new_row = {"click":0, "visit":1, "group":"control"}
         df0 = df0.append( new_row, ignore_index=True )
     else:
-        new_row = {"click":0, "visit":1, "group":"control"}
+        new_row = {"click":0, "visit":1, "group":"treatment"}
         df0 = df0.append( new_row, ignore_index=True )
 
     df0.to_csv( "data_experiment.csv", index=False)
